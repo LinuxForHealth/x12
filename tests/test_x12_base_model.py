89a78f0444cc1a58891436a23ca59fda96bc1875
Dixon Whitmire <dixonwh@gmail.com>
2021-06-15 15:08:28 -0400

    black formatting

--- tests/test_x12_base_model.py
+++ tests/test_x12_base_model.py
@@ -14,10 +14,12 @@ def x12_mock_model():
     """
     :return:A Mock model which extends X12BaseModel
     """
+
     class MockModel(X12BaseModel):
         """
         A simple model used to test X12BaseModel features.
         """
+
         first_name: str
         last_name: str
         address: Optional[List[str]]
@@ -27,7 +29,7 @@ def x12_mock_model():
         "delimiters": {
             "element_separator": "*",
             "repetition_separator": "^",
-            "segment_terminator": "~"
+            "segment_terminator": "~",
         },
         "loop_context": {
             "id": "Mock ID",
@@ -37,21 +39,26 @@ def x12_mock_model():
         "first_name": "JOHN",
         "last_name": "DOE",
         "address": ["1400 ANYHOO LANE", "PLEASANTVILLE", "SC", "90210"],
-        "birth_date": datetime.date(year=1980, month=1, day=1)
+        "birth_date": datetime.date(year=1980, month=1, day=1),
     }
 
-    return  MockModel(**fields)
+    return MockModel(**fields)
 
 
 def test_x12(x12_mock_model):
     """
     Tests the X12BaseModel's x12() method
     """
-    assert x12_mock_model.x12() == "MCK*JOHN*DOE*1400 ANYHOO LANE^PLEASANTVILLE^SC^90210*19800101~"
+    assert (
+        x12_mock_model.x12()
+        == "MCK*JOHN*DOE*1400 ANYHOO LANE^PLEASANTVILLE^SC^90210*19800101~"
+    )
 
     x12_mock_model.address = None
     assert x12_mock_model.x12() == "MCK*JOHN*DOE**19800101~"
 
     x12_mock_model.address = ["1400 ANYHOO LANE", "PLEASANTVILLE", "SC", "90210"]
     x12_mock_model.birth_date = None
-    assert x12_mock_model.x12() == "MCK*JOHN*DOE*1400 ANYHOO LANE^PLEASANTVILLE^SC^90210~"
+    assert (
+        x12_mock_model.x12() == "MCK*JOHN*DOE*1400 ANYHOO LANE^PLEASANTVILLE^SC^90210~"
+    )
--- x12/segments/__init__.py
+++ x12/segments/__init__.py
@@ -11,6 +11,7 @@ class X12Loop(BaseModel):
     """
     Models a X12 loop, which is a logical grouping of segments within a transaction set.
     """
+
     id: str
     description: str
 
@@ -19,6 +20,7 @@ class X12Delimiters(BaseModel):
     """
     X12Delimiters models the common delimiters used within a X12 message's segments.
     """
+
     element_separator: str = Field(min_length=1, max_length=1)
     repetition_separator: str = Field(min_length=1, max_length=1)
     segment_terminator: str = Field(min_length=1, max_length=1)
@@ -28,6 +30,7 @@ class X12BaseModel(BaseModel):
     """
     X12BaseModel serves as the base class for all X12 segment models.
     """
+
     delimiters: X12Delimiters
     loop_context: X12Loop
     segment_name: str = Field(min_length=2, max_length=3)
@@ -36,7 +39,9 @@ class X12BaseModel(BaseModel):
         """
         :return: the X12 representation of the model instance
         """
-        model_values = [v for v in self.dict(exclude={"delimiters", "loop_context"}).values()]
+        model_values = [
+            v for v in self.dict(exclude={"delimiters", "loop_context"}).values()
+        ]
         x12_values = []
         for v in model_values:
             if isinstance(v, list):
@@ -51,5 +56,7 @@ class X12BaseModel(BaseModel):
             else:
                 x12_values.append(v)
 
-        x12_str = self.delimiters.element_separator.join(x12_values).rstrip(self.delimiters.element_separator)
+        x12_str = self.delimiters.element_separator.join(x12_values).rstrip(
+            self.delimiters.element_separator
+        )
         return x12_str + self.delimiters.segment_terminator
