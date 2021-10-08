eb362c32a0918cf4c2ee5ae0f25ab112ea87980b
Dixon Whitmire <dixonwh@gmail.com>
2021-10-08 15:10:08 -0400

    837 properties
    Signed-off-by: Dixon Whitmire <dixonwh@gmail.com>

--- src/tests/healthcare_claim_professional/test_837_005010X222A2.py
+++ src/tests/healthcare_claim_professional/test_837_005010X222A2.py
@@ -42,3 +42,42 @@ def test_837_commercial_insurance_subscriber(
         assert (
             model_result[0].x12() == x12_837_commercial_insurance_subscriber_transaction
         )
+
+
+def test_837_subscriber_properties(x12_837_commercial_insurance_subscriber_input):
+    """
+    Test 837 properties where the patient is the subscriber
+    """
+    with X12ModelReader(x12_837_commercial_insurance_subscriber_input) as r:
+        model_result = [m for m in r.models()]
+        assert len(model_result) == 1
+
+        model = model_result[0]
+
+        nm1_segment = model.patient["loop_2010ba"]["nm1_segment"]
+        assert nm1_segment["name_last_or_organization_name"] == "SMITH"
+        assert nm1_segment["name_first"] == "JANE"
+
+        claim_data = model.claim
+        assert claim_data["clm_segment"] is not None
+        assert len(claim_data["sv1_segment"]) == 4
+
+
+def test_837_dependent_properties(x12_837_commercial_insurance_dependent_input):
+    """
+    Test 837 properties where the patient is the subscriber
+    """
+    with X12ModelReader(x12_837_commercial_insurance_dependent_input) as r:
+        model_result = [m for m in r.models()]
+        assert len(model_result) == 1
+
+        model = model_result[0]
+        assert "loop_2300" not in model
+
+        nm1_segment = model.patient["loop_2010ca"]["nm1_segment"]
+        assert nm1_segment["name_last_or_organization_name"] == "SMITH"
+        assert nm1_segment["name_first"] == "TED"
+
+        claim_data = model.claim
+        assert claim_data["clm_segment"] is not None
+        assert len(claim_data["sv1_segment"]) == 4
